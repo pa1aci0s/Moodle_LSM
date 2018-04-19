7edc95f8eb01a1ed182141ca0038926d06fb1143
pa1aci0s <37189322+pa1aci0s@users.noreply.github.com>
2018-04-19 14:52:46 -0500

    Add files via upload

---
+++ loginsystem/header.php
@@ -0,0 +1,46 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet"type=text/css href="style.css">
+</head>
+<body>
+
+	<header>
+		<nav>
+			<div class="main-wrapper">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+				</ul>
+				<div class="nav-login">
+					<?php 
+					if (isset($_SESSION['u_id'])){
+						echo '<form action="includes/logout.inc.php" method="POST">
+						<button type="submit" name="submit">Logout</button></form>';
+
+					} else {
+						echo'<form action ="includes/login.inc.php" method="POST">
+					<input type="text" name="uid" placeholder="Username">
+					<input type="text" name="pwd" placeholder="Password" >
+					<button type="submit" name="submit">Login</button>
+				</form>';
+
+					}
+
+
+					?>
+
+
+
+				
+
+				</div>
+			</div>
+		</nav>
+	</header>
---
+++ loginsystem/includes/login.inc.php
@@ -0,0 +1,49 @@
+<?php
+include 'dbh.inc.php';
+session_start();
+
+if(isset($_POST['submit'])){
+
+	
+	$uid = mysqli_real_escape_string($conn, $_POST['uid']);
+	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+	//Error handlers
+
+	if (empty($uid) || empty($pwd)){
+		header("Location: ../index.php?login=empty");
+			exit();
+
+	} else{
+		$sql ="SELECT * FROM users WHERE user_uid='$uid'";
+		$result = mysqli_query($conn, $sql);
+		$result = mysqli_num_rows($result);
+		if ($resultCheck < 1) {
+			header("Location: ../index.php?login=error");
+			exit();
+		} else{
+			if ($row = mysqli_fetch_assoc($result)) {
+				//De-hasshing the password
+				$hashedPwdCheck = password_verify($pwd, $row['user_pwd']);
+				if ($hashedPwdCheck == false) {
+					header("Location: ../index.php?login=error");
+					exit();
+				} elseif ($hashedPwdCheck == true) {
+					//Login user here
+					$_SESSION['u_id'] = $row['user_id'];
+					$_SESSION['u_first'] = $row['user_id'];
+					$_SESSION['u_last'] = $row['user_id'];
+					$_SESSION['u_email'] = $row['user_id'];
+					$_SESSION['u_uid'] = $row['user_uid'];
+					header("Location: ../index.php?login=success");
+					exit();
+				}
+			}
+		}
+	}
+
+} else {
+		header("Location: ../index.php?login=error");
+		exit();
+	}
+
